4587ce1fcc26398c55bd49e537b50012b106229e
Dennis Buduev <dbuduev@gmail.com>
2016-05-14 13:16:18 +1200

    V3.

--- lib/leanpoker_elixir/player.ex
+++ lib/leanpoker_elixir/player.ex
@@ -1,7 +1,17 @@
 defmodule LeanpokerElixir.Player do
-  @version "0.0.2"
+  @version "0.0.3"
   def bet_request(game_state) do
-    game_state["current_buy_in"]
+    %{
+      "in_action" => in_action,
+      "dealer" => dealer,
+      "players" => players,
+      "current_buy_in" => current_buy_in,
+    } = game_state
+    if rem(dealer, length players) == in_action or rem(dealer + 1, length players) == in_action do
+      current_buy_in
+    else
+      0
+    end
   end
 
   def showdown(game_state) do
